dba795cb4ed7bc116befd98dbe641a03fb76994e
mike <mike@DESKTOP-2JKV4QP.>
2025-04-17 15:53:10 +0300

    done

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,12 @@
+// src/__tests__/utils.test.js
+
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
     const birthYear = 1984;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    const currentYear = new Date().getFullYear();
+    expect(ageOfPerson).toBe(currentYear - birthYear);
   });
 });
